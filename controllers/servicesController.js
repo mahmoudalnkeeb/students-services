2dec7a615ff5e12ea2b1ce8451f99301268da57b
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2023-03-03 15:09:20 +0200

    servicesController , servicesRouter DONE

---
+++ controllers/servicesController.js
@@ -0,0 +1,107 @@
+const { request, response } = require('express');
+const services = require('../models/Service');
+
+/*--------------
+  @param {request} req
+  @param {response} res
+  @param {*} next
+
+async function name(req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+}
+---------------*/
+
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ * @param {*} next
+ */
+
+async function getServices(req, res, next) {
+  try {
+    let allServices = await services.getServices();
+    res.status(200).json(allServices);
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ * @param {*} next
+ */
+
+async function getOneService(req, res, next) {
+  try {
+    let { id } = req.params;
+    let service = await services.getOneService(id);
+    res.status(200).json(service);
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ * @param {*} next
+ */
+
+async function createService(req, res, next) {
+  try {
+    let { name, desc } = req.body;
+    let service = await services.createService(name, desc);
+    res.status(201).json(service);
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ * @param {*} next
+ */
+
+async function updateService(req, res, next) {
+  try {
+    let { name, desc } = req.body;
+    let { id } = req.params;
+    let service = await services.updateService(id, name, desc);
+    res.status(200).json(service);
+  } catch (error) {
+    next(error);
+  }
+}
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ * @param {*} next
+ */
+
+async function deleteService(req, res, next) {
+  try {
+    let { id } = req.params;
+    let service = await services.deleteService(id);
+    res.status(204).json(service);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  getServices,
+  getOneService,
+  createService,
+  updateService,
+  deleteService,
+};
--- routes/services.js
+++ routes/services.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
+const servicesController = require('../controllers/servicesController');
 
 //-- TODO LIST --
-// get services [ ]
-// get service by id [ ]
-// add service [ ]
-// update service [ ]
-// delete service [ ]
+// get services [DONE]
+router.get('/all', servicesController.getServices);
+// get service by id [DONE]
+router.get('/:id', servicesController.getOneService);
+// add service [DONE]
+router.post('/add', servicesController.createService);
+// update service [DONE]
+router.put('/edit/:id', servicesController.updateService);
+// delete service [DONE]
+router.put('/delete/:id', servicesController.deleteService);
 
 module.exports = router;
